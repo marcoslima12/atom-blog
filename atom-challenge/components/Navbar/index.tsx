f8955706bf7679e184a6240ccae3e0ae2378982d
Marcos Antonio <marcosantonio-10@live.com>
2023-09-02 16:00:27 -0300

    feature/header: page structure

---
+++ atom-challenge/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export const Navbar = () => {
+  return (
+    <Container>
+      <h2>Nabar</h2>
+    </Container>
+  );
+};
---
+++ atom-challenge/pages/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #290742;
+  padding: 4.25rem 8.55rem;
+`;
+ 
